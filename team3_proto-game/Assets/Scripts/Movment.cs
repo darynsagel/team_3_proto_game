8f91f7e468bb956f97be2cade9b155100b393a1b
WaffyWaffle <ryan134690@gmail.com>
2019-10-01 19:08:02 -0400

    Add files via upload
    movement for the enemy

---
+++ team3_proto-game/Assets/Scripts/Movment.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    public float speed;
+    public bool faceRight= true ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision .gameObject.tag  == "Wall")
+        {
+            if(faceRight ==true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                faceRight = false;
+                Debug.Log("jhi");
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                faceRight = true;
+                Debug.Log("Hello");
+            }
+        }
+    }
+}
