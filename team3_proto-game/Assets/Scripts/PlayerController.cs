39aa8c7a60a67466239a15a4b3cb58e1a2f2927d
Daryn Sagel <daryn.asagel@gmail.com>
2019-10-01 17:36:24 -0400

    Eels added

--- team3_proto-game/Assets/Scripts/PlayerController.cs
+++ team3_proto-game/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	public float speed;
+
+	private Rigidbody2D rb;
+	private Vector2 moveVelocity;
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
+		rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		moveVelocity = moveInput.normalized * speed;
     }
+
+    void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+	}
 }
