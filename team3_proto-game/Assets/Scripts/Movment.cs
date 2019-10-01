475b6f86d2c378896ca8ef36b66b56e5d74cf421
WaffyWaffle <ryan134690@gmail.com>
2019-10-01 19:09:18 -0400

    Add files via upload
    updated version of movement

--- team3_proto-game/Assets/Scripts/Movment.cs
+++ team3_proto-game/Assets/Scripts/Movment.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Movment : MonoBehaviour
 {
-    public float speed;
+    public float speed = 3;
     public bool faceRight= true ;
     // Start is called before the first frame update
     void Start()
