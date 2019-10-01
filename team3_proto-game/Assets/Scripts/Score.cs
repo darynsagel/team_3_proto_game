eb660207fc1a3b6f9c51480b253bff18f6904c8a
WaffyWaffle <ryan134690@gmail.com>
2019-10-01 19:59:42 -0400

    Add files via upload
    A score system

---
+++ team3_proto-game/Assets/Scripts/Score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+
+    public int score=0;
+    public Text scoreBoard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ruby")
+        {
+            score++;
+            scoreBoard.text = "Score: " + score;
+        }
+    }
+}
